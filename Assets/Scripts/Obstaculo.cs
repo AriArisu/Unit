e27dba6a2246d815c6b589bc576f8d97b7b6458a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-27 11:22:58 -0300

    coiso

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -7,6 +7,13 @@ public class obstaculo : MonoBehaviour
     [SerializeField]
     private float velocidade = 1.9f;
 
+    [SerializeField]
+    private float varairposicaoY;
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-varairposicaoY,varairposicaoY));
+}
     // Update is called once per frame
     void Update()
     {
